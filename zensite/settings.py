b0f0ce86837e208d2458bd44c111f59782388c2c
Minar <minar.rhman@gmail.com>
2021-08-08 18:46:52 +0600

    updated settings

--- zensite/settings.py
+++ zensite/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '*lgtga@y)d(22t9rwz9an%)^-#0k)!)k*6q_9*b4xcjkg@63uk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'zensite.herokuapp.com']
 
